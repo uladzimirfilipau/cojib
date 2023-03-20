0cd62705d1f02ce5964e54af5b6afce2443354bb
Vova <uladzimirfilipau@gmail.com>
2023-03-20 20:57:43 +0300

    refactor: Lead

--- src/components/Lead/Lead.jsx
+++ src/components/Lead/Lead.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import leadImage from '../../images/cojib-image.jpg';
+import { servicelinks } from '../../utils/consts';
 import './Lead.css';
 
 function Lead() {
+  const listItems = servicelinks.map(({ id, title, link }) => (
+    <li key={id} className='lead__list-item'>
+      <NavLink to={link} className='lead__link'>
+        {title}
+      </NavLink>
+    </li>
+  ));
+
   return (
     <section className='lead'>
-      <img src={leadImage} alt='Лилия Филиппова' class='lead__image' />
+      <img src={leadImage} alt='Лилия Филиппова' className='lead__image' />
 
       <article className='lead__article'>
         <h1 className='lead__title'>Профессиональный парикмахер-стилист</h1>
         <p className='lead__title'>с&nbsp;15-летним опытом&nbsp;работы.</p>
 
-        <ul className='lead__list'>
-          <li className='lead__list-item'>Стрижка волос</li>
-          <li className='lead__list-item'>Окрашивание волос</li>
-          <li className='lead__list-item'>Укладка волос</li>
-          <li className='lead__list-item'>Уход за волосами</li>
-        </ul>
+        <ul className='lead__list'>{listItems}</ul>
       </article>
     </section>
   );
