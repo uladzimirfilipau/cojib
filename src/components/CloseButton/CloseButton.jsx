3205b2a68e75cccc391ce4686f98657ff115c37b
Vova <uladzimirfilipau@gmail.com>
2023-03-21 18:56:20 +0300

    feat: CloseButton

---
+++ src/components/CloseButton/CloseButton.jsx
@@ -0,0 +1,7 @@
+import './CloseButton.css';
+
+function CloseButton({ onClose }) {
+  return <button type='button' aria-label='Close' className='close-button' onClick={onClose} />;
+}
+
+export default CloseButton;
