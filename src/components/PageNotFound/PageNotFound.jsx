b10c1c624eed412608d39ef83e7e22f4fc0ced88
Vova <uladzimirfilipau@gmail.com>
2023-03-28 16:32:09 +0300

    feat: PageNotFound

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,19 @@
+import './PageNotFound.css';
+import { NavLink } from 'react-router-dom';
+
+function PageNotFound() {
+  return (
+    <section className='notfound'>
+      <h1 className='notfound__title'>404</h1>
+      <h2 className='notfound__subtitle'>Ой, такая страница не найдена =('_')=</h2>
+      <p className='notfound__text'>
+        Возможно Вы сделали запрос к несуществующей странице или эта страница находится в разработке
+      </p>
+      <NavLink to='/' className='notfound__link'>
+        Вернуться на главную
+      </NavLink>
+    </section>
+  );
+}
+
+export default PageNotFound;
