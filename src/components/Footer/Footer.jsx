24e9ce14b875fada4c0e50171e937108f599f5cf
Vova <uladzimirfilipau@gmail.com>
2023-03-29 14:32:00 +0300

    feat: Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css';
+import FooterTop from '../FooterTop/FooterTop';
+import FooterBottom from '../FooterBottom/FooterBottom';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <FooterTop />
+      <FooterBottom />
+    </footer>
+  );
+}
+
+export default Footer;
