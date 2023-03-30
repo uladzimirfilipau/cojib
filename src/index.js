a68505ce85656b02421ec6472ea0b65b346be32b
Vova <uladzimirfilipau@gmail.com>
2023-03-30 12:04:35 +0300

    feat: ScrollToTop

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from '../src/components/ScrollToTop/ScrollToTop';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/cojib'>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
