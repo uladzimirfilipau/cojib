5ceda06298149484bd7d2572b5344f0c474c41f7
Vova <uladzimirfilipau@gmail.com>
2023-04-03 17:37:22 +0300

    remove: CloseButton

--- src/components/CloseButton/CloseButton.jsx
+++
@@ -1,7 +0,0 @@
-import './CloseButton.css';
-
-function CloseButton({ onClose }) {
-  return <button type='button' aria-label='Close' className='close-button' onClick={onClose} />;
-}
-
-export default CloseButton;
