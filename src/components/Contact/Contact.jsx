eebdaf54a225f28356c96b1672eb9a06e5456c43
Vova <uladzimirfilipau@gmail.com>
2023-04-07 10:22:23 +0300

    feat: Contact

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import Message from '../Message/Message';
+import Questions from '../Questions/Questions';
+
+export default function Contact() {
+  return (
+    <>
+      <Questions />
+      <Message />
+    </>
+  );
+}
