9c821d02ab9fb069629b0032947a0afcc016a248
Vova <uladzimirfilipau@gmail.com>
2023-04-10 22:07:34 +0300

    feat: Question

---
+++ src/components/Question/Question.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './Question.css';
+
+export default function Question({ item }) {
+  const { subtitle, text } = item;
+  const [showText, setShowText] = useState(false);
+
+  const toggleClick = () => {
+    setShowText(!showText);
+  };
+
+  const buttonClassName = `questions__button ${showText && 'questions__button_active'}`;
+  const buttonTextClassName = `questions__button-text ${
+    showText && 'questions__button-text_active'
+  }`;
+
+  return (
+    <li className='questions__item' onClick={toggleClick}>
+      <button type='button' className={buttonClassName}>
+        <span className={buttonTextClassName}>+</span>
+      </button>
+      <article className='questions__article'>
+        <h3 className='questions__subtitle'>{subtitle}</h3>
+        {showText && <p className='questions__text'>{text}</p>}
+      </article>
+    </li>
+  );
+}
