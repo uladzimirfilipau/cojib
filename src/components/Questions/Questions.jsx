7704d7e701a3224f6c0d0f4ca9f861a3e312a581
Vova <uladzimirfilipau@gmail.com>
2023-07-17 19:47:47 +0300

    refactor: Question

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -7,7 +7,7 @@ export default function Questions() {
 
   return (
     <section className='questions' aria-label='Ответы на вопросы и контакты для связи'>
-      <h2 className='questions__title'>Часто задаваемые вопросы</h2>
+      <h1 className='questions__title'>Часто задаваемые вопросы</h1>
       <ul className='questions__list'>{questions}</ul>
     </section>
   );
